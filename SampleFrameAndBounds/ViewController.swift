5f0a452413b9c26b3d6b216fd09a71aa333d580d
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-13 10:57:02 +0900

    Initial commit

---
+++ SampleFrameAndBounds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleFrameAndBounds
+//
+//  Created by Johnny Toda on 2023/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
